36e7385ee11480236971977fabec6f1891b9259d
shontee <shontee@gmail.com>
2013-05-27 14:57:32 +0800

    decrese buffer

--- bro-admin/home.php
+++ bro-admin/home.php
@@ -15,12 +15,12 @@ function call_video(){
 	 flashvars.userid= "<?php echo $_SESSION['jigowatt']['username']; ?>"; 
 	 flashvars.sessionid= "<?php echo $_SESSION['jigowatt']['sessionid']; ?>";
 	 swfobject.embedSWF(
-	 "images/listener0017.swf",
+	 "images/listener0018.swf",
 	 "flashcontent",
 	 "550",
 	 "400",
 	 "9.0.0",
-	 "images/listener0017.swf",
+	 "images/listener0018.swf",
 	 flashvars,
 	 {"userid": "<?php echo $_SESSION['jigowatt']['username']; ?>",
 	 "sessionid": "<?php echo $_SESSION['jigowatt']['sessionid']; ?>"});
