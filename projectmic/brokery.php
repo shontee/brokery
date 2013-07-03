ac8399f760a68febec15a254f4d624128d4d215f
shontee <shontee@gmail.com>
2013-07-03 15:22:54 +0800

    add link

--- projectmic/brokery.php
+++ projectmic/brokery.php
@@ -10,14 +10,17 @@ session_write_close();
 	<head>
 		<title>Brokery</title>
 		<link href="front_images/_index/adv_en.css" rel="stylesheet" type="text/css" />
-<link href="front_images/_index/main_en.css" rel="stylesheet" type="text/css" />
+		<link href="front_images/_index/main_en.css" rel="stylesheet" type="text/css" />
+	<script language="javascript">
+		
+	</script>
 	</head>
 	<body>
 		<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" WIDTH="1060" HEIGHT="760" id="Yourfilename" ALIGN="">
-			<PARAM NAME=movie VALUE="front_images/brokery003.swf"> 
+			<PARAM NAME=movie VALUE="front_images/brokery0703.swf"> 
 			<PARAM NAME=quality VALUE=high> 
 			<PARAM NAME=bgcolor VALUE=#333399> 
-		<EMBED src="front_images/brokery003.swf" quality=high bgcolor=#333399 WIDTH="1060" HEIGHT="760" NAME="Yourfilename" ALIGN="" TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED> 
+		<EMBED src="front_images/brokery0703.swf" quality=high bgcolor=#333399 WIDTH="1060" HEIGHT="760" NAME="Yourfilename" ALIGN="" TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED> 
 		</OBJECT>
 	</body>
 </html>
